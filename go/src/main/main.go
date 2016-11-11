538064db576c44d7cea7b5253d0ebdac61845f47
ktrinanm <ktrinanl@gmail.com>
2016-11-11 13:01:01 -0700

    TRying to get setup

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"users"
+	"fmt"
+)
+
 func main()  {
     fmt.Println("Main running.")
+
+	a := User{"uid", "fname", "lname", "pword"}
+
+	fmt.Println(a.fname)
 }
--- go/src/users/users.go
+++ go/src/users/users.go
@@ -1,1 +1,15 @@
 package users
+
+import (
+	io/ioutil
+)
+
+const data_folder = "C:\Users\Katrina\Workspace\product\go\src\users\data"
+
+type User struct{
+	Uid string
+	fname string
+	lname string
+	pword string
+	
+}
