f82730c332e37283f76c735d0e2347e20adba678
Tyler Mumford <tyler.mumford@icloud.com>
2016-11-12 00:31:29 -0700

    Fix error in build script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ GOPATH=/vagrant/go go build -o /vagrant/bin/controller main
 cd /vagrant/cs/
 mcs Input.cs Argument.cs Motion.cs Vote.cs Discuss.cs Amend.cs Table.cs -out:/vagrant/bin/storage
 
-./pug.sh
+/vagrant/scripts/pug.sh
