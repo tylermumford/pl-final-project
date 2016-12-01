796209e10682cd9d76c1b7d7b5040041c18bb5de
Tyler Mumford <tyler.mumford@icloud.com>
2016-12-01 09:31:36 -0700

    Get rid of unintended server output.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,3 @@ GOPATH=/vagrant/go go build -o /vagrant/bin/controller main
 
 cd /vagrant/cs/
 mcs Input.cs Argument.cs Motion.cs Vote.cs Discuss.cs Amend.cs Table.cs -out:/vagrant/bin/storage
-
-mkdir /vagrant/data/users
-mkdir /vagrant/data/storage
