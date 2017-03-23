cf22c24ce9de22e8a763daabee408df69d37fb26
ktrinanm <ktrinanl@gmail.com>
2017-03-23 14:07:22 -0600

    added list of comments

--- go/src/savvie/storage/comments.go
+++ go/src/savvie/storage/comments.go
@@ -18,6 +18,156 @@ type Comment struct {
 	Type   string // "Comment", "Pro", or "Con".
 }
 
+var commentList = []Comment{
+	Comment{
+		"johnsmith",
+		"78912",
+		time.Now(),
+		"Come into work on our day off",
+		"Con",
+	},
+	Comment{
+		"tess-terson",
+		"78912",
+		time.Now(),
+		"No reason, work or other, to miss the meeting",
+		"Pro",
+	},
+	Comment{
+		"mitchmasters",
+		"89123",
+		time.Now(),
+		"less likely for us to be exhausted from the week.",
+		"Pro",
+	},
+	Comment{
+		"janitha",
+		"89123",
+		time.Now(),
+		"Not everyone is at work by then!",
+		"Con",
+	},
+	Comment{
+		"anonymous",
+		"89123",
+		time.Now(),
+		"Even if I'm at work, I may not be mentally acute yet",
+		"Con",
+	},
+	Comment{
+		"mysharona",
+		"89123",
+		time.Now(),
+		"This is going to get in the way of productive work",
+		"Con",
+	},
+	Comment{
+		"kylejenkins",
+		"67891",
+		time.Now(),
+		"Not everyone likes popcorn, or the same flavor",
+		"Con",
+	},
+	Comment{
+		"tylermumford",
+		"67891",
+		time.Now(),
+		"popcorn + rootbeer is the tastiest!!",
+		"Pro",
+	},
+	Comment{
+		"katrinamehring",
+		"67891",
+		time.Now(),
+		"But what about my figure????",
+		"Con",
+	},
+	Comment{
+		"leeeerooooyyy-jeeeeeeenkiiiins",
+		"67891",
+		time.Now(),
+		"It'll make us feel parched the rest of the day",
+		"Con",
+	},
+	Comment{
+		"banananana",
+		"45678",
+		time.Now(),
+		"What flavor of cookies makes a difference. Not oatmeal raisin!",
+		"Comment",
+	},
+	Comment{
+		"jessicajones",
+		"45678",
+		time.Now(),
+		"Baked goods are healthier",
+		"Pro",
+	},
+	Comment{
+		"jonesy",
+		"23456",
+		time.Now(),
+		"We're sitting all day, why would we want to sit for longer",
+		"Con",
+	},
+	Comment{
+		"shannon",
+		"23456",
+		time.Now(),
+		"Safe from weather",
+		"Pro",
+	},
+	Comment{
+		"bonnie",
+		"12345",
+		time.Now(),
+		"fresh air",
+		"Pro",
+	},
+	Comment{
+		"clyde",
+		"12345",
+		time.Now(),
+		"It is springtime, might as well",
+		"Comment",
+	},
+	Comment{
+		"anderson",
+		"12345",
+		time.Now(),
+		"Weather is hard to predict",
+		"Con",
+	},
+	Comment{
+		"safetydance",
+		"12345",
+		time.Now(),
+		"It would be good to change it up",
+		"Comment",
+	},
+	Comment{
+		"stacysmom",
+		"34567",
+		time.Now(),
+		"Wouldnt' be great for people that can't stand for long periods of time",
+		"Con",
+	},
+	Comment{
+		"santa",
+		"34567",
+		time.Now(),
+		"Very distracting to see everyone moving and fidgetting",
+		"Con",
+	},
+	Comment{
+		"lucky",
+		"34567",
+		time.Now(),
+		"It's similar to the option to sit in a meeting room",
+		"Comment",
+	},
+}
+
 // NiceDate returns an output-ready, human-recognizable date string.
 // The format of the date is not guaranteed to remain constant. It may change without notice.
 func (c *Comment) NiceDate() string {
